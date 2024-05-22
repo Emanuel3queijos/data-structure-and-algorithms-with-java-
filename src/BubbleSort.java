d2a6feff5fff71b383007cf9624d601cbd394788
Emanuel <101946589+Emanuel3queijos@users.noreply.github.com>
2024-05-22 19:14:39 -0300

    bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+Integer array[] = {2,5,3,6,7,4,8,};
+Integer aux;
+Boolean salva;
+
+for (int i = 0; i < array.length; i++){
+    for (int j = 0; j < (array.length-1); j++){
+        if (array[j] > array[j+1]){
+            aux = array[j+1];
+            array[j+1] = array[j];
+            array[j] = aux;
+        }
+    }
+}
+for (int i = 0; i< array.length; i++){
+    System.out.println(array[i]);
+}
+
+    }
+}
---
+++ src/arrays/readme.md
@@ -0,0 +1,8 @@
+# Arrays
+
+> a array its a simple data structure, used to save a collection of same data types elements  
+
+>the size of an array is defined in your creation. 
+
+
+
